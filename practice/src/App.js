a3ab547846bc3ccba1278705b305e49ef6ad2272
unknown <jreidmke@gmail.com>
2020-11-12 09:43:07 -0600

    refactoring to allow inheritance

--- practice/src/App.js
+++ practice/src/App.js
@@ -7,7 +7,21 @@ import './App.css';
 
 function App() {
   return (
-
+    <div>
+      <h1>Shopping Cart</h1>
+        <div>
+          {items.map(i => (
+            <div>
+              <h4>{i.name}</h4>
+              <ul>
+                <li>Price: {i.price}</li>
+                <li>Qty: {i.qty}</li>
+              </ul>
+            </div>
+          ))}
+          <h4>Total: {items.map(i=>i.price * i.qty).reduce((a, b) => a+b)}</h4>
+        </div>
+    </div>
   );
 }
 
