0dbaaa8d254a997c1d624410d0ba58ed2f81ae38
unknown <jreidmke@gmail.com>
2020-11-12 09:44:25 -0600

    import sc from module

--- practice/src/App.js
+++ practice/src/App.js
@@ -2,26 +2,13 @@
 // import {Add, Multiply} from './helpers';
 // import char, {Hi} from './swChar';
 import items from './items';
+import ShoppingCart from './shoppingCart';
 import './App.css';
 
 
 function App() {
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-        <div>
-          {items.map(i => (
-            <div>
-              <h4>{i.name}</h4>
-              <ul>
-                <li>Price: {i.price}</li>
-                <li>Qty: {i.qty}</li>
-              </ul>
-            </div>
-          ))}
-          <h4>Total: {items.map(i=>i.price * i.qty).reduce((a, b) => a+b)}</h4>
-        </div>
-    </div>
+    <ShoppingCart/>
   );
 }
 
