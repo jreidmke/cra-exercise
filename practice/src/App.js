3d2f1c3cd9bf7a0afdd23a698dcc25d41bf33b0e
unknown <jreidmke@gmail.com>
2020-11-12 09:32:26 -0600

    comment out boiler plate

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import {Add, Multiply} from './helpers';
 // import char, {Hi} from './swChar';
+import items from './items';
 import './App.css';
 
 
 function App() {
-
   return (
 
   );
